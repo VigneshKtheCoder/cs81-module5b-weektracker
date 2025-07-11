8233472aef86a0db323c730f84a4d89bc15c4c97
Vignesh Kalyanasundaram <vignesh20092001@gmail.com>
2025-07-11 11:47:29 -0700

    array

---
+++ activityTracker.js
@@ -0,0 +1,9 @@
+const myWeek = [
+  { day: "Monday", activity: "Drawing", category: "creative", hoursSpent: 1.5, enjoyment: 3, timeOfDay: "evening" },
+  { day: "Tuesday", activity: "Running", category: "physical", hoursSpent: 1, enjoyment: 5, timeOfDay: "morning" },
+  { day: "Wednesday", activity: "Gaming", category: "social", hoursSpent: 2, enjoyment: 10, timeOfDay: "evening" },
+  { day: "Thursday", activity: "Writing", category: "creative", hoursSpent: 1.25, enjoyment: 6, timeOfDay: "afternoon" },
+  { day: "Friday", activity: "Cooking", category: "creative", hoursSpent: 1, enjoyment: 9, timeOfDay: "evening" },
+  { day: "Saturday", activity: "Texting", category: "social", hoursSpent: 2.5, enjoyment: 10, timeOfDay: "morning" },
+  { day: "Sunday", activity: "Reading", category: "creative", hoursSpent: 1.2, enjoyment: 7, timeOfDay: "afternoon" }
+];
